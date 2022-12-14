a169067f2b0bfe5f9fdf07fa01fc6b962913ac65
Ziven <>
2022-12-14 15:05:06 +0800

    0.1.5

--- src/Api/Controller/DeleteAllHistoryController.php
+++ src/Api/Controller/DeleteAllHistoryController.php
@@ -4,7 +4,7 @@ namespace Ziven\viewHistory\Api\Controller;
 
 use Flarum\Api\Controller\AbstractDeleteController;
 use Psr\Http\Message\ServerRequestInterface;
-use Ziven\viewHistory\Model\viewHistory;
+use Ziven\viewHistory\Model\ViewHistory;
 
 class DeleteAllHistoryController extends AbstractDeleteController{
     protected function delete(ServerRequestInterface $request){
